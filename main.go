5a1ef2e563fa243e2384f0558f883aa0408487fc
andywei1996 <andy1996andy@gmail.com>
2017-07-06 11:47:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -49,9 +49,9 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
-			message := event.Message.(type)
-// 			switch  {
-// 			case *linebot.TextMessage:
+			
+			switch  message := event.Message.(type){
+			case *linebot.TextMessage:
 // 				if message.Text == "幹" {
 // 					err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" 幹!")).Do()
 // 				}
@@ -66,7 +66,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" 幹!")).Do(); err != nil {
 					log.Print(err)
 				}
-// 			}
+			}
 		}
 	}
 }
